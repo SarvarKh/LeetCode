10f103ae5aa4a450092ca5fadf5d1ec5669d96ed
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-12-08 17:42:12 +0500

    Time: 86 ms (22.56%), Space: 209.9 MB (65.41%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.rb
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.rb
@@ -1,7 +1,26 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_min(nums)
-    nums.min
+    return nums[0] if nums.size == 1
+    
+    left = 0
+    right = nums.length - 1
+    
+    return nums[0] if nums[right] > nums[0]
+    
+    while right >= left
+        mid = left + (right - left) / 2
+        
+        return nums[mid + 1] if nums[mid] > nums[mid + 1]
+        
+        return nums[mid] if nums[mid - 1] > nums[mid]
+        
+        if nums[mid] > nums[0]
+            left = mid + 1
+        else
+            right = mid - 1
+        end
+    end
 end
 
 nums = [3,4,5,1,2]
