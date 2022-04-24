58394ec4e6e0e2f1c24b42c50fe405aee3390bcd
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-04-24 11:48:01 +0500

    Time: 33 ms (7.84%), Space: 46.4 MB (17.00%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -2,18 +2,18 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
         
-        HashMap<Character, Integer> ranMap = new HashMap<>();
-        HashMap<Character, Integer> magMap = new HashMap<>();
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        HashMap<Character, Integer> tMap = new HashMap<>();
         
         for (int i=0; i<s.length(); i++) {
             char m = s.charAt(i);
             char r = t.charAt(i);
-            ranMap.put(r, ranMap.getOrDefault(r, 0) +1);
-            magMap.put(m, magMap.getOrDefault(m, 0) +1);
+            sMap.put(r, sMap.getOrDefault(r, 0) +1);
+            tMap.put(m, tMap.getOrDefault(m, 0) +1);
         }
         
-        for (Character c : ranMap.keySet()) {
-            if(ranMap.get(c) > magMap.getOrDefault(c, -1)) return false;
+        for (Character c : sMap.keySet()) {
+            if(sMap.get(c) > tMap.getOrDefault(c, -1)) return false;
         }
         
         return true;
