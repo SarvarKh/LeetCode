8f39f92eb9fc0a6acc7d8c2fc8d0686fc3d42e9a
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-04-24 11:39:48 +0500

    Time: 22 ms (33.01%), Space: 52.3 MB (20.04%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -2,21 +2,17 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         if(ransomNote.length() > magazine.length()) return false;
         
-        HashMap<Character, Integer> ranMap = new HashMap<>();
         HashMap<Character, Integer> magMap = new HashMap<>();
         
         for (int i=0; i<magazine.length(); i++) {
-            char m = magazine.charAt(i);
-            if (i < ransomNote.length()) {
-                char r = ransomNote.charAt(i);
-                ranMap.put(r, ranMap.getOrDefault(r, 0) +1);
-            }
-            
+            char m = magazine.charAt(i);    
             magMap.put(m, magMap.getOrDefault(m, 0) +1);
         }
         
-        for (Character c : ranMap.keySet()) {
-            if(ranMap.get(c) > magMap.getOrDefault(c, -1)) return false;
+        for (char c : ransomNote.toCharArray()) {
+            if(!magMap.containsKey(c) || magMap.get(c) <= 0) return false;
+            
+            magMap.put(c, magMap.get(c)-1);
         }
         
         return true;
